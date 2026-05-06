f70e11ea125f0fbfc79d54e2364334e41ec312ae
RTspec <rahultudu022@gmail.com>
2026-05-06 22:10:16 +0530

    Time: 5 ms (82.97%), Space: 77.3 MB (68.28%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxArea(int[] height) {
+        //width = difference between two indexes
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length-1;
+
+        while(left < right){
+            int length = Math.min(height[left],height[right]);
+            int width = right - left;
+            int currentArea = length * width;
+
+            maxArea = Math.max(maxArea,currentArea);
+
+            if(height[left] < height[right]){
+        
+                left++;
+            }
+            else{
+            right--;
+            }
+
+        }
+        return maxArea; 
+    }
+}
